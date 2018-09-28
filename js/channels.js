a748695efe44337070e96d135991e9e60672a82d
sbhrepo <sbhrepo@users.noreply.github.com>
2018-09-28 12:52:02 -0400

    challenge 7 week 4

---
+++ js/channels.js
@@ -0,0 +1,44 @@
+var Yummy = {
+    name:"Yummy",
+    createdOn:'April 1, 2016',
+    createdBy:"minus.plus.yummy",
+    starred:false,
+    expiresIn:100,
+    messageCount:999
+};
+
+var SevenContinents = {
+    name:"SevenContinents",
+    createdOn:'April 1, 2016',
+    createdBy:"cheeses.yard.applies",
+    starred:true,
+    expiresIn:100,
+    messageCount:999
+};
+
+var KillerApp = {
+    name:"KillerApp",
+    createdOn:'April 1, 2016',
+    createdBy:"minus.plus.yummy",
+    starred:false,
+    expiresIn:100,
+    messageCount:999
+};
+
+var FirstPersonOnMars = {
+    name:"FirstPersonOnMars",
+    createdOn:'April 1, 2016',
+    createdBy:"minus.plus.yummy",
+    starred:true,
+    expiresIn:100,
+    messageCount:999
+};
+
+var Octoberfest = {
+    name:"Octoberfest",
+    createdOn:'April 1, 2016',
+    createdBy:"minus.plus.yummy",
+    starred:false,
+    expiresIn:100,
+    messageCount:999
+};
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 /* #6 start the #external #action and say hello */
 console.log("App is alive");
+var currentChannel;
+var currentLocation = "kicks.pasta.steer";
 
 /**
  * #6 #Switcher function for the #channels name in the right app bar
@@ -7,26 +9,32 @@ console.log("App is alive");
  */
 function switchChannel(channelName) {
     //Log the channel switch
-    console.log("Tuning in to channel", channelName);
+    console.log("Tuning in to channel", '#'+channelName.name);
+    currentChannel = channelName;
 
     //Write the new channel to the right app bar
-    document.getElementById('channel-name').innerHTML = channelName;
+    document.getElementById('channel-name').innerHTML = '#'+ channelName.name;
 
     //#6 change the #channel #location
-    document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>';
+    document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>'+channelName.createdBy+'</strong></a>';
 
     /* #6 #liking channels on #click */
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star-o.png');
+    /* $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star-o.png'); */
+    /* $('#channel-star').html('<i class="far fa-star" onclick="star()"></i>'); */
+    $('#channel-star').html('<i class='+(channelName.starred?"\"fas fa-star\"" : "\"far fa-star\"")+' onclick="star()"></i>');
 
     /* #6 #highlight the selected #channel.
        This is inefficient (jQuery has to search all channel list items), but we'll change it later on */
     $('#channels li').removeClass('selected');
-    $('#channels li:contains(' + channelName + ')').addClass('selected');
+    $('#channels li:contains(' + channelName.name + ')').addClass('selected');
+
 }
 
 /* #6 #liking a channel on #click */
 function star() {
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star.png');
+    /* $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star.png'); */
+    $('#channel-star').find('i').toggleClass('far fa-star fas fa-star');
+    currentChannel.starred = !currentChannel.starred;
 }
 
 /**
